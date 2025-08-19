50a1c0d5d5f0f176b0427576e85755fb53313f7e
Đoàn Việt <you@example.com>
2025-08-19 17:27:31 +0700

    update

--- src/index.js
+++ src/index.js
@@ -5,21 +5,22 @@ const handlebars = require('express-handlebars');
 const app = express();
 const port = 3000;
 
-app.use(morgan('combined'));
+const route = require('./routes/index')
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+// app.use(morgan('combined'));
 
 app.engine('hbs', handlebars.engine(
   {extname: '.hbs'}
 ));
 app.set('view engine', 'hbs');
-app.set('views',path.join(__dirname, 'resources/views'))
+app.set('views',path.join(__dirname, 'resources/views'));
 
-app.get('/', (req, res) => {
-  res.render ('home');
-})
+route(app);
 
-app.get('/news', (req, res) => {
-  res.render ('news');
-})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
